2cdc11dc05c0250fb24e938c5b6ea062495c6fed
irwin <irwinlkl@gmail.com>
2022-08-23 01:01:12 +0800

    commit

--- character.py
+++ character.py
@@ -20,45 +20,45 @@ class GameCharacter:
         else:
             self.setup_mage()
     
-
+    # setting up units
     def setup_warrior(self):
         self.type = "warrior"
         self.atk = random.randint(5,20)
         self.df = random.randint(1,10)
+        self.exp = 0
         
-    def get_attack(self):
-        return self.atk
-
     def setup_tanker(self):
         self.type = "tanker"
         self.atk = random.randint(1,10)
         self.df = random.randint(5,15)
-        
-
+        self.exp = 0
 
     def setup_mage(self):
         self.type = "mage"
-        self.atk = random.randint(5,30)
+        self.atk = random.randint(20,30)
         self.df = random.randint(1,5)
-        
+        self.exp = 0
 
     def attack(self,target):
         damage = self.atk - target.df + random.randint(-5, 10)
-        target.hp -= damage
+        # TODONE: implement fix for negative damage
+        if damage <= 0:
+            target.hp -= 1   
+            
+        else:
+            target.hp -= damage
+             
+        print(self.name+" did "+str(damage)+" damage")
+        print("\n")
+        
+        
 
     def __str__(self):
         text = self.type+" "+self.name+",hp:"+str(self.hp)
         text += ", atk:" + str(self.atk)+", df" + str(self.df)
         return text   
 
-
-
-
-    '''out put list of 
-        ai units == player units
-        ai units will be selected randomly
-        ai name prefix "AI(two digit)"
-
-    '''
-
+    def get_damage(self):
+        pass
+        
 
