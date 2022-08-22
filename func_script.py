345ce640e33387a5ae7c223c26ef175bf38af605
irwin <irwinlkl@gmail.com>
2022-08-23 01:09:47 +0800

    backup

--- func_script.py
+++ func_script.py
@@ -4,16 +4,55 @@ import random
 
 def setup_ai(len_player_units):
     ai_lis = []
-    type_lis = ["w","t","w"]
+    type_lis = ["w","t","m"]
     for i in range(len_player_units):
-        ai_lis.append(character.GameCharacter(type_lis[random.randint(0,3)],"AI"+str(random.randint(10,100))))
+        ai_lis.append(character.GameCharacter(type_lis[random.randint(0,2)],"AI" + str(random.randint(0,9)) + str(random.randint(0,9) )))
     return ai_lis
 
 def setup_player(num_unit):
     player_units = []
     for i in range(num_unit):
 
-        player_unit_type = str(input('choose unit t / w / m = tanker / warrior / mage: '))
+        player_unit_type = str(input('choose unit '+str(i+1)+' t / w / m = tanker / warrior / mage: '))
         player_unit_name = str(input("give your unit a name: "))
         player_units.append(character.GameCharacter(player_unit_type,player_unit_name))
     return player_units    
+
+def update_player_units(number_of_player_unit,player_units):
+    for i in range(number_of_player_unit):
+            print(player_units[i].__str__(),end=" / ")
+
+def update_ai_units(number_of_ai_unit,ai_units):
+    for i in range(number_of_ai_unit):
+            print(ai_units[i].__str__(),end=" / ")
+
+
+def clear_screen():
+    print("\n"*10)
+
+
+def show_game_board(player_units,ai_units):
+    print("Player unit")
+    update_player_units(len(player_units),player_units)
+    print("\n")
+    print("Ai units")
+    update_ai_units(len(ai_units),ai_units)
+    print("\n")
+
+def check_hp_is_zero(units):
+   
+    for i in range(len(units)):
+        hp = units[i].hp
+        if hp <= 0:
+            return True
+
+def dead_unit_index(units):
+   
+    for i in range(len(units)):
+        hp = units[i].hp
+        if hp <= 0:
+            return i
+
+
+
+    
--- main.py
+++ main.py
@@ -1,33 +1,132 @@
-import func_script
+import func_script as fuc
 import character 
-unit = []
+import random
+
+
 rounds = 1
 player_units = []
 ai_units = []
 
 
+
 def main():
+    
     exit = "x"
     end = ""
+    ai_life = 0
+    player_life = 0
+    player_unit = 0
+    ai_unit = 0
     
     while exit != end:
-        #select_player_units()
-        num_unit = int(input("select number of units: "))
-        player_units = func_script.setup_player(num_unit)
-        ai_units = func_script.setup_ai(len(player_units))
-        '''
+        global rounds
         
-        setup_ai_units()
-        player_attack_ai_unit() / ai_attack_player()
+        #setting up player units in a list
+        num_unit = int(input("select number of units: "))
+        player_units = fuc.setup_player(num_unit)
+
+        
+        # setting up ai units in a list
+        ai_units = fuc.setup_ai(len(player_units))
+
+
+       #
+        player_unit_to_atk = 0
+        ai_unit_to_atk = 0
+
+       
+
+        #end loop when player or ai run out of units / life
+        while True:
+            
+            fuc.clear_screen()
+            print(len(ai_units))
+            
+
+
+            #setting up turn base attack player go first
+            if rounds % 2 == 1 :
+                
+                
+                #show selected units and ai units 
+                fuc.show_game_board(player_units,ai_units)
+            
+                #@@@@@@player can select a unit to atk the other unit@@@@@
+
+                #player select which of their own unit to attack
+                player_unit_to_atk = int(input("select unit to attack (1 for 1st unit, 2 for 2nd unit etc...: ") )
+                player_unit_to_atk -= 1
+                #catch out of range input default to first unit
+                if player_unit_to_atk < 0 or player_unit_to_atk > len(player_units):
+                    player_unit_to_atk = 0
+                
+                
+                #player select which ai unit to attack
+                ai_unit_to_atk = int(input("select ai_unit to attack (1 for 1st unit, 2 for 2nd unit etc...: "))
+                ai_unit_to_atk -= 1
+                if ai_unit_to_atk < 0 or ai_unit_to_atk > len(ai_units):
+                #catch out of range input and default to first unit
+                    ai_unit_to_atk = 0
+
+                fuc.clear_screen()
+                
+                #process atk calculation
+                player_units[player_unit_to_atk].attack(ai_units[ai_unit_to_atk])
+                fuc.show_game_board(player_units,ai_units)
+
+                #if player unit die remove unit from list 
+                
+                if fuc.check_hp_is_zero(player_units) == True:
+
+                    dead_unit = fuc.dead_unit_index(player_units)
+                    player_units.pop(dead_unit)
+                
+                #check if player win
+                if len(ai_units)<= 0:
+                    print("player WIN")
+                    break
+
+                
+
+            else:
+                
+                ai_units[random.randint(0,len(ai_units)-1)].attack(player_units[random.randint(0,len(ai_units)-1)])  
+                fuc.show_game_board(player_units,ai_units)  
+
+                #if ai unit die remove unit from list 
+                
+                if fuc.check_hp_is_zero(ai_units) == True:
+
+                    dead_unit = fuc.dead_unit_index(ai_units)
+                    ai_units.pop(dead_unit)
+                #check if ai win
+                if len(player_units)<= 0:
+                    print("Ai WIN")
+                    break
+
+
+            
+            
+            
+            
+            
+            #print(ai_life)
+            #print(player_life)
+            #print(ahp,php)
+            #print(rounds)
+            rounds += 1
+            p = str(input("press enter to continue:"))
+            
+        '''
+        update game state
+        
+        
         check player or ai win
         print event logs
 
         
         '''
-        for i in range(len(player_units)):
-            print(player_units[i].__str__())
-        print("\n")
-        for i in range(len(player_units)):
-            print(ai_units[i].__str__())    
+        
+        
         end = str(input("x to end"))   
 main()
